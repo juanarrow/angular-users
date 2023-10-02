d54a4a67a1718225f0cf989b42afc986661479b7
Juan A. García <juan.a.garcia.gomez@gmail.com>
2023-10-02 22:16:57 +0200

    New User Service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { User } from './user';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { ToastController, ToastOptions } from '@ionic/angular';
 import { UserInfoFavClicked } from './user-info/user-info.component';
+import { UsersService } from '../users.service';
 
 
 @Component({
@@ -13,29 +14,19 @@ import { UserInfoFavClicked } from './user-info/user-info.component';
 })
 export class HomePage implements OnInit {
  
-  private _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  public users$:Observable<User[]> = this._users.asObservable();
-
-  
-  
+  public loading:boolean = false;
   constructor(
     private router:Router,
-    private toast:ToastController
+    private toast:ToastController,
+    public users:UsersService
   ) {
   }
 
   ngOnInit(): void {
-    
-    var users:User[] = [
-      {id: 1, name:"Juan A.", surname:"garcía gómez", age:46, fav:true},
-      {id: 2, name:"Alejandro", surname:"garcía gómez", age:45, fav:true},
-      {id: 3, name:"juan", surname:"garcía Valencia", age:4, fav:false},
-      {id: 4, name:"María del Mar", surname:"Valencia Valencia", age:46, fav:true},
-      {id: 5, name:"Lydia", surname:"garcía Robles", age:11, fav:false}
-    ];
-    this._users.next(users);
-    
-   
+    this.loading = true;
+    this.users.getAll().subscribe(users=>{
+      this.loading = false;
+    });
   }
 
   public welcome(){
@@ -44,28 +35,25 @@ export class HomePage implements OnInit {
 
 
   public onFavClicked(user:User, event:UserInfoFavClicked){
-    //recibimos en user el usuario asociado a la tarjeta
-    //recimos en event un objeto del tipo UserInfoFavClicked que tiene una propiedad fav que indica si hay que añadir o eliminar de la lista de favoritos
-    //creamos una copia del array actual de usuarios
-    const users = [...this._users.value];
-    //buscamos el índice del usuario para modificar su propiedad fav
-    var index = users.findIndex((_user)=>_user.id == user.id);
-    if(index>=0)
-      //actualizamos la propiedad fav con el valor que hemos recibido por el evento
-      users[index].fav = event.fav??false; //en el caso de que fav sea undefined devolvemos falso.
-    //notificamos un nuevo array de usuarios para que se renderice en la plantilla
-    this._users.next([...users]); 
-    //Notificamos con un Toast que se ha pulsado
-    const options:ToastOptions = {
-      message:`User ${event.fav?'added':'removed'} ${event.fav?'to':'from'} favourites`, //mensaje del toast
-      duration:1000, // 1 segundo
-      position:'bottom', // el toast se situa en la parte inferior
-      color:'danger', // color del toast
-      cssClass:'fav-ion-toast' //Una clase que podemos poner en global.scss para configurar el ion-toast
-    };
-
-    //creamos el toast
-    this.toast.create(options).then(toast=>toast.present());
+    var _user:User = {...user};
+    _user.fav = event.fav??false; //en el caso de que fav sea undefined devolvemos falso.
+    this.users.updateUser(_user).subscribe(
+        {next: user=>{
+        //Notificamos con un Toast que se ha pulsado
+        const options:ToastOptions = {
+          message:`User ${event.fav?'added':'removed'} ${event.fav?'to':'from'} favourites`, //mensaje del toast
+          duration:1000, // 1 segundo
+          position:'bottom', // el toast se situa en la parte inferior
+          color:'danger', // color del toast
+          cssClass:'fav-ion-toast' //Una clase que podemos poner en global.scss para configurar el ion-toast
+        };
+        //creamos el toast
+        this.toast.create(options).then(toast=>toast.present());
+        },
+        error: err=>{
+          console.log(err);
+        }
+      });
   }
 
   public async onCardClicked(){
