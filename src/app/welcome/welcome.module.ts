97145e859bdb312fc9dc29cd65236a47fae78b7e
Juan A. García <juan.a.garcia.gomez@gmail.com>
2023-10-09 20:33:59 +0200

    new swiper on welcome

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -5,15 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { WelcomePageRoutingModule } from './welcome-routing.module';
-
 import { WelcomePage } from './welcome.page';
+import {SwiperModule} from 'swiper/angular';
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Navigation, Pagination, Scrollbar, A11y]);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    WelcomePageRoutingModule
+    WelcomePageRoutingModule,
+    SwiperModule
   ],
   declarations: [WelcomePage]
 })
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EventsParams, SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-welcome',
@@ -12,4 +13,20 @@ export class WelcomePage implements OnInit {
   ngOnInit() {
   }
 
+  async onSlideClicked(index:number){
+    this.swiper?.swiperRef.slideTo(index);
+  }
+  
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+  slideNext(){
+    this.swiper?.swiperRef.slideNext();
+  }
+  slidePrev(){
+    this.swiper?.swiperRef.slidePrev();
+  }
+
+  onSwiper(event:any) {
+  
+    console.log(event);
+  }
 }
